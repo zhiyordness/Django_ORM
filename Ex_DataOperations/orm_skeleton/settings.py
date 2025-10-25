f6037a5b5bdfd79840325ae1190fc253a94ba8ad
Zhivomir Yordanov <zhivomiryordanov@gmail.com>
2025-10-25 13:04:39 +0300

    initial commit

--- Ex_DataOperations/orm_skeleton/settings.py
+++ Ex_DataOperations/orm_skeleton/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -85,7 +86,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
