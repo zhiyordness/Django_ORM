f0f0f0484ffb47fdbb0435a416f75ed17d794b3e
Zhivomir Yordanov <zhivomiryordanov@gmail.com>
2025-10-25 14:12:12 +0300

    Pet

--- Ex_DataOperations/caller.py
+++ Ex_DataOperations/caller.py
@@ -6,5 +6,24 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-
+from main_app.models import Pet
 # Create queries within functions
+
+def create_pet(name: str, species: str) -> str:
+    pet = Pet(name=name, species=species)
+    pet.save()
+    return f"{name} is a very cute {species}!"
+
+
+# print(create_pet(name='Fido', species='Dog'))
+# print(create_pet(name='Whiskers', species='Cat'))
+# print(create_pet(name='Rocky', species='Turtle'))
+# print(create_pet(name='Goldie', species='Fish'))
+# print(create_pet(name='Polly', species='Parrot'))
+# print(create_pet(name='Buddy', species='Dog'))
+# print(create_pet(name='Mittens', species='Cat'))
+# print(create_pet(name='Spike', species='Hedgehog'))
+# print(create_pet(name='Bubbles', species='Fish'))
+# print(create_pet(name='Rex', species='Dinosaur'))
+
+
